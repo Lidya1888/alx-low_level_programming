8aa3ce11acce746b658269cca51144faf8a90727
Lidya1888 <lidyagenene@gmail.com>
2022-03-21 14:30:40 -0700

    corrected

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,29 +1,19 @@
 #include "main.h"
-/**
- * _atoi - int
- * @s: pointer
- * Return: int.
- */
-int _atoi(char *s)
-{
-	int i;
-	int res = 0;
-	int sig = -1;
-	int brk = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+/**
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
 	{
-		if (s[i] == '-')
-			sig = sig * -1;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			res = res * 10;
-			res -= (s[i] - '0');
-			brk = 1;
-		}
-		else if (brk == 1)
-			break;
+		s++;
+		l++;
 	}
-	res = sig * res;
-	return (res);
+	return (l);
 }
